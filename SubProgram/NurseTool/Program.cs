dced350a3bed0f0ad64119b87102c7a65ec05be2
apporoad <apporoadina@163.com>
2017-03-12 13:41:26 +0800

    modify

--- SubProgram/NurseTool/Program.cs
+++ SubProgram/NurseTool/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,90 @@ namespace NurseTool
     {
         static void Main(string[] args)
         {
+            switch (args[0])
+            {
+                case "mssend":
+                    if (args.Length > 2)
+                    {
+                        TestSend(args[1], args[2]);
+                    }
+                    else
+                    {
+                        TestSend(args[1]);
+                    }
+                    break;
+                case "mscount":
+                    TestGetCount(args[1]);
+                    break;
+                case "msallcount":
+                    TestAllCount();
+                    break;
+                default:
+                    Console.WriteLine("nothing todo ");
+                    break;
 
+            }
 
+        }
+
+        public static void TestSend(string mq, string strMsg = "test")
+        {
+            string strMq = mq;//@".\Private$\lxy2";
+            MessageQueue mqQue = new MessageQueue(strMq);
+            mqQue.MessageReadPropertyFilter.SetAll();
+
+            System.Messaging.Message msg = new System.Messaging.Message();
+            //消息主体
+            msg.Body = strMsg + "|" + DateTime.Now.ToString();
+            //用描述设置ID
+            msg.Label = "id123";
+            //将消息加入到发送队列
+            msg.ResponseQueue = mqQue;
+            msg.AttachSenderId = true;
+
+            msg.Formatter = new System.Messaging.XmlMessageFormatter(new Type[] { typeof(string) });
+
+            try
+            {
+                //发送
+                mqQue.Send(msg);
+                Console.WriteLine("发送Msg成功:" + msg.Body);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+        public static void TestGetCount(string instance)
+        {
+            try
+            {
+                PerformanceCounterCategory category = new PerformanceCounterCategory("MSMQ Queue");
+
+                PerformanceCounter counter = new PerformanceCounter("MSMQ Queue", "Messages in Queue", instance);
+                counter.InstanceName = instance;
+                Console.WriteLine(string.Format("{0} 数量：{1}", counter.InstanceName, counter.NextValue().ToString()));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("错误:" + ex.ToString());
+            }
+
+        }
+
+        public static void TestAllCount()
+        {
+            PerformanceCounterCategory countCategory = new PerformanceCounterCategory("MSMQ Queue");
+
+            //所有消息队列数量
+            PerformanceCounter allCount = new PerformanceCounter("MSMQ Queue", "Messages in Queue");
+            foreach (string instanceName in countCategory.GetInstanceNames())
+            {
+                allCount.InstanceName = instanceName;//需要给实例名赋值
+                Console.WriteLine(string.Format("{0} 数量：{1}", allCount.InstanceName, allCount.NextValue().ToString()));
+            }
         }
     }
 }
--- SubProgram/NurseTool/readMe.txt
+++ SubProgram/NurseTool/readMe.txt
@@ -1,8 +1,14 @@
 ﻿
 
 
-//
+//如何测试 sendMsg
+NurseTool.exe mssend xyliu\private$\lxy
 
+//如何测试 mscount
+NurseTool.exe mscount xyliu\private$\lxy
 
-msmq 无法连接时
+//如何测试 msallcount
+NurseTool.exe msallcount
+
+msmq count无法连接时
 LODTCR /R
